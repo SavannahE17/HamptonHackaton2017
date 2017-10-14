1a56d1664f338cc5c1fbeb0566ccc5594c70a197
Carlos Mayers <c@carlosmayers.com>
2017-10-14 16:14:04 -0400

    Took out tiles bug and made actors name the name of image

--- core/src/com/hampton/game/demo/MatchingGame.java
+++ core/src/com/hampton/game/demo/MatchingGame.java
@@ -96,7 +96,7 @@ public class MatchingGame extends GameScreen {
                 @Override
                 public void touchDown(InputEvent event, float x, float y, int pointer, int button) {
                     pictures.get(index).setVisible(true);
-                    tiles[i].setVisible(false);
+                    //tiles[i].setVisible(false);
 
                 }
             });
