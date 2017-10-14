a053b40839096f8171633707094afeaf48055b12
donne5899 <donnell.debnam@my.hamptonu.edu>
2017-10-14 16:18:29 -0400

    Background

--- core/src/com/hampton/game/demo/MatchingGame.java
+++ core/src/com/hampton/game/demo/MatchingGame.java
@@ -39,7 +39,7 @@ public class MatchingGame extends GameScreen {
 
     @Override
     public void createActors() {
-        backgroundColor = new Color(1, 1, 1, 1);
+        backgroundColor = new Color(.8726f, .6372f, .2718f, 1);
         tiles = new Actor[12];
 
         pictures = new Array<Actor>(12);
